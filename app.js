5798c2b3bf406635a459c57c7c34accec1e4ee86
Seong Hyun Han <seong.han@mohawkcollege.ca>
2018-02-02 16:50:18 -0500

    Finishing Authenticate with Google +

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const authRoutes = require('./routes/auth-route');
+const profileRoutes = require('./routes/profile-route');
 const passportSetup = require('./config/passport-setup');
+const keys = require('./config/keys');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,12 +14,28 @@ const port = process.env.PORT || 3000;
 // set up view engine
 app.set('view engine', 'ejs');
 
+// intialize cookie session
+app.use(cookieSession({
+    maxAge: 24* 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, (req, res) => {
+    console.log('Connected to Mongodb');
+});
+
 // set up routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 // create home route
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {user: req.user});
 });
 
 app.listen(port, () => {
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,14 +1,42 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
-const  keys = require('./keys');
+const keys = require('./keys');
+const User = require('../models/user-model');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
     // options for the google strategy
     callbackURL:'/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-}, () => {
+}, (accessToken, refreshToken, profile, done) => {
     // passport callback function
-    
+    // check if user already exists in our db
+    User.findOne({googleId: profile.id}).then((user) =>{
+        if(user){
+            // already have the user
+            console.log(`User is: ${user}`);
+            done(null, user);
+        }else{
+            // if not, create user in our db
+            new User({
+                username: profile.displayName,
+                googleId: profile.id,
+                thumbnail: profile._json.image.url
+            }).save().then((newUser) => {
+                console.log(`new user created: ${newUser}`);
+                done(null, newUser);
+            });    
+        }
+    });
 }));
